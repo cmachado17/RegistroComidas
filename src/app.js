35d136cf2cb7271d22b9966ae4cf265a763e51bb
Cristian Machado <56597872+cmachado17@users.noreply.github.com>
2023-12-03 18:10:25 -0300

    maquetado, navegacion y primeros estilos

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function showHistory() {
+    document.getElementById('form-section').style.display = 'none';
+    document.getElementById('list-section').style.display = 'block';
+}
+
+function showForm() {
+    document.getElementById('form-section').style.display = 'block';
+    document.getElementById('list-section').style.display = 'none';
+}
